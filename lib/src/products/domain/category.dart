db97796e3b0f312b66775881dc97af59765105a0
Francisco Pitriqueo <franpitri@gmail.com>
2021-10-09 20:25:09 -0300

    ✨ feat: add product categories

---
+++ lib/src/products/domain/category.dart
@@ -0,0 +1,12 @@
+// If categories start changing we should refactor this into a proper entity.
+enum Category {
+  frozenFood,
+  groceryProducts,
+  babyProducts,
+  alcoholicBeverages,
+  nonAlcoholicBeverages,
+  freshFood,
+  cleaningProducts,
+  petProducts,
+  personalCare,
+}
--- lib/src/src.dart
+++ lib/src/src.dart
@@ -1,5 +1,6 @@
 export 'auth/auth.dart';
 export 'di/di.dart';
+export 'products/products.dart';
 export 'profiles/profiles.dart';
 export 'shared/shared.dart';
 export 'social_auth/social_auth.dart';

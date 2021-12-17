d4cc679b07a0eac46773369afd8ad3ea8e1bdffc
Francisco Pitriqueo <franpitri@gmail.com>
2021-12-17 01:02:32 -0300

    ♻️ refactor: update api path

--- lib/src/configs/constants.dart
+++ lib/src/configs/constants.dart
@@ -1,5 +1,4 @@
 class Constants {
-  static const apiBaseUrl =
-      'http://6ac2-2803-9800-9883-4926-dbc-dfe0-7e0c-d872.ngrok.io/api/'; //'https://api.baratito.app/api/';
+  static const apiBaseUrl = 'https://api.baratito.app/api/';
   static const defaultPageSize = 25;
 }
